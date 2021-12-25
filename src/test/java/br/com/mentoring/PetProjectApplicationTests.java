c2c27b89f01dda19afaba7d6e3f938f0e2161436
Ana Paula <ana.slima6@gmail.com>
2021-12-25 14:31:27 -0300

    first commit

---
+++ src/test/java/br/com/mentoring/PetProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mentoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
